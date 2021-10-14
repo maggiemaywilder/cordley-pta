ceaa171173c195c89e1c09001428ed516ec906c0
Maggie May Wilder <maggiemaywilder@gmail.com>
2021-10-14 11:47:45 -0500

    ititial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Cordley Elementary PTA</h1>
+      </header>
+      <main>
+        <h3>I am a body.</h3>
+      </main>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return 
+            <div className="header">
+                <h1>Cordley Elementary PTA</h1>
+            </div>;
+    }
+}
+
+export default Header;
