e30d48c3e9ecc1732940b6797dec24256559c0cd
Maggie May Wilder <maggiemaywilder@gmail.com>
2021-10-16 12:50:26 -0500

    basic header toggler details later

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,28 @@
 import React, {Component} from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+import '../App.css';
 
 class Header extends Component {
     render() {
-        return 
-            <div className="header">
-                <h1>Cordley Elementary PTA</h1>
-            </div>;
+        return (
+            <Navbar className="topNav navbar-dark" expand="lg">
+                <Container >
+                    <Navbar.Brand href="/" className="text-white"><h1>Cordley Elementary PTA</h1></Navbar.Brand>
+                    {/* <Container className="justify-content-end text-end"  >
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="text-white" />
+                    <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+
+                    <Nav >
+                        <Nav.Link href="#home" className="text-white">Home</Nav.Link>
+                        <Nav.Link href="#link" className="text-white">Link</Nav.Link>
+                    </Nav>
+                    </Navbar.Collapse>
+                    </Container> */}
+                </Container>
+            </Navbar>
+
+        );
     }
 }
 
